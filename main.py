e9cf4a79ad4647aa595d34afa63ae6b8aaaca868
Sarjak404notfound <bhandarisarjak@gmail.com>
2024-01-06 20:59:29 +0545

    Tkinter initializations

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+import sqlite3
+
+
+app=tk.Tk()
+app.title("ID generator")
+app.iconbitmap("assets/main.ico")
+app.geometry("800x600")
+app.resizable(False,False)
+main=Frame(app)
+bgimg=tk.PhotoImage("/assets/bklg.jpg")
+bgimg_Label=Label(main,image=bgimg)
+bgimg_Label.pack()
+main.place(x=0,y=0)
+app.mainloop()
 
 
 
