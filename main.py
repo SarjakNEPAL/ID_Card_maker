b8b5c57b66a5972a6027f2752efc1af8319d1670
Sarjak-Desktop <bhandarisarjak@gmail.com>
2024-01-09 20:53:01 +0545

    Login and Registration done

--- main.py
+++ main.py
@@ -6,100 +6,164 @@ from PIL import ImageTk, Image
 import sqlite3
 
 
-global s
-s=True
+def scene1():
+    link= sqlite3.connect("Database.db")
+    cursor=link.cursor()
 
-app=tk.Tk()
-app.title("ID generator")
-app.iconbitmap("assets/main.ico")
-app.geometry("1000x500")
-app.resizable(False,False)
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                username TEXT NOT NULL PRIMARY KEY,
+                password TEXT NOT NULL
+            )
+    ''')
 
-
-#login frame
-main=Frame(app,width=1000,height=500)
-main.config(bg="#5585b5")
-main.place(x=0,y=0)
-
-#Canvas framings
-Ad=Frame(main,width=400,height=480)
-Mfrm=Frame(main,width=500, height=200,bg="#53a8b6")
-creator=Frame(main,width=500,height=50,bg="#53a8b6")
-Tit=Frame(main,width=500,height=70,bg="#bbe4e9")
-subti=Frame(main,width=500,height=50,bg="#bbe4e9")
-Tit.place(x=450,y=30)
-subti.place(x=450,y=110)
-Ad.place(x=10,y=35)
-Mfrm.place(x=450,y=170)
-creator.place(x=450,y=440)
-
-#imagings
-Ad_img=Image.open("assets/id-card.png")
-ad_pic=ImageTk.PhotoImage(Ad_img.resize((400, 440)))
-adlabel=Label(Ad,image=ad_pic,bg="#79c2d0")
-adlabel.pack()
-
-#exit func
-def ext():
-    app.destroy()
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user_info (
+                username TEXT NOT NULL,
+                gender TEXT NOT NULL,
+                phone INTEGER NOT NULL UNIQUE,
+                address TEXT,
+                company TEXT
+            )      
+    ''')
 
 
-#Default Prompts
-title=Label(Tit,text="ID-Generator",font=('Helvetica', 30),bg="#bbe4e9")
-subtitle=Label(subti,text="Login",font=('Helvetica', 25),bg="#bbe4e9")
-title.place(x=150,y=6)
-subtitle.place(x=200,y=5)
 
-def checkfld():
-    username=user1.get()
-    password=pass1.get()
-    if username !='':
-        if password!='' and len(password)>7:
-            messagebox.showinfo("Stage 1 check",f"Hi {username} Login page will now exit")
-        else:
-            messagebox.showwarning("Password error","Password must contain more than 7 characters")
-    else:
-        messagebox.showwarning("Warning!","Username must not be blank")
+    def dataget(a):
+        username=user1.get()
+        if a==("p"):
+            cursor.execute('SELECT password FROM users WHERE username=?',[username])
+        if a==("i"):
+            cursor.execute('SELECT * FROM user_info WHERE username=?',[username])
+        re=cursor.fetchone()
+        return re
 
-def reg():
-    def regst():
-        messagebox.showinfo("Registration complete")
+
+
+
+    app=tk.Tk()
+    app.title("ID generator")
+    app.iconbitmap("assets/main.ico")
+    app.geometry("1000x500")
+    app.resizable(False,False)
+
     def lg():
         subtitle.config(text="Login")
-        Bttn2.config(text="Register", command=reg, bg="Black")
+        Bttn2.config(text="Register", command=reg)
         Bttn.config(text="Login",command=checkfld)
-    subtitle.config(text="Register")
-    Bttn.config(text="Register", command=regst)
-    Bttn2.config(text="Login",command=lg)
+
+    def lgmn(u,p):
+        a=dataget("p")
+        print(a)
+        if a[0]==p:
+            print("Loggedin")
+            app.destroy()
+        else:
+            messagebox.showerror("CREDENTIAL ERROR","Username or Password error")
+        pass1.delete(0,END)
+
+    def signup():
+        username=user1.get()
+        password=pass1.get()
+        print(username,password)
+        if username !='':
+            if password!='' and len(password)>7:
+                cursor.execute('SELECT username FROM users WHERE username=?',[username])
+                if cursor.fetchone() is not None:
+                    messagebox.showerror('Error',"Username already exists")
+                else:
+                    cursor.execute('INSERT INTO users VALUES(?,?)',[username,password])
+                    link.commit()
+
+                    messagebox.showinfo('Success',"Account has been created")         
+                    lg()       
+            else:
+                messagebox.showwarning("Password error","Password must contain more than 7 characters")
+        else:
+            messagebox.showwarning("Warning!","Username must not be blank")
+        user1.delete(0,END)
+        pass1.delete(0,END)
+    #login frame
+    main=Frame(app,width=1000,height=500)
+    main.config(bg="#5585b5")
+    main.place(x=0,y=0)
+
+    #Canvas framings
+    Ad=Frame(main,width=400,height=480)
+    Mfrm=Frame(main,width=500, height=200,bg="#53a8b6")
+    creator=Frame(main,width=500,height=50,bg="#53a8b6")
+    Tit=Frame(main,width=500,height=70,bg="#bbe4e9")
+    subti=Frame(main,width=500,height=50,bg="#bbe4e9")
+    Tit.place(x=450,y=30)
+    subti.place(x=450,y=110)
+    Ad.place(x=10,y=35)
+    Mfrm.place(x=450,y=170)
+    creator.place(x=450,y=440)
+
+    #imagings
+    Ad_img=Image.open("assets/id-card.png")
+    ad_pic=ImageTk.PhotoImage(Ad_img.resize((400, 440)))
+    adlabel=Label(Ad,image=ad_pic,bg="#79c2d0")
+    adlabel.pack()
+
+    #exit func
+    def ext():
+        app.destroy()
 
 
-#Default entries
-crtr=Label(creator,text="|||| Created By - Sarjak Bhandari ||||",font=('Helvetica', 20),bg="#bbe4e9")
-usrnmr_=Label(Mfrm,text="Username:",font=('Helvetica', 25),bg="#53a8b6")
-pss_=Label(Mfrm,text="Password:",font=('Helvetica', 25),bg="#53a8b6")
-Bttn=Button(Mfrm,text="Login",bg="#ececec",height=1,width=10,font=('Helvetica', 20),command=checkfld)
-Bttn2=Button(Mfrm,text="Register",bg="#ececec",height=1,width=10,font=('Helvetica', 20),command=reg)
-Button(main,text="Exit",command=ext,font=('Helvetica', 20),bg="#bbe4e9",width=30,height=1).place(x=455,y=380)
-#inputs
-global username
-global password
+    #Default Prompts
+    title=Label(Tit,text="ID-Generator",font=('Helvetica', 30),bg="#bbe4e9")
+    subtitle=Label(subti,text="Login",font=('Helvetica', 25),bg="#bbe4e9")
+    title.place(x=150,y=6)
+    subtitle.place(x=200,y=5)
 
+    def checkfld():
+        username=user1.get()
+        password=pass1.get()
+        if username !='':
+            if password!='' and len(password)>7:
+                lgmn(username,password)
+            else:
+                messagebox.showwarning("Password error","Password must contain more than 7 characters")
+        else:
+            messagebox.showwarning("Warning!","Username must not be blank")
 
+    
+    def reg():
+        subtitle.config(text="Register")
+        Bttn.config(text="Register", command=signup)
+        Bttn2.config(text="Login",command=lg)
 
-user1=Entry(Mfrm,font=('Helvetica', 20))
-pass1=Entry(Mfrm,font=('Helvetica', 20))
+    #Default entries
+    crtr=Label(creator,text="|||| Created By - Sarjak Bhandari ||||",font=('Helvetica', 20),bg="#bbe4e9")
+    usrnmr_=Label(Mfrm,text="Username:",font=('Helvetica', 25),bg="#53a8b6")
+    pss_=Label(Mfrm,text="Password:",font=('Helvetica', 25),bg="#53a8b6")
+    Bttn=Button(Mfrm,text="Login",bg="#ececec",height=1,width=10,font=('Helvetica', 20),command=checkfld)
+    Bttn2=Button(Mfrm,text="Register",bg="#ececec",height=1,width=10,font=('Helvetica', 20),command=reg)
+    Button(main,text="Exit",command=ext,font=('Helvetica', 20),bg="#bbe4e9",width=30,height=1).place(x=455,y=380)
+    #inputs
+    user1=Entry(Mfrm,font=('Helvetica', 20))
+    pass1=Entry(Mfrm,font=('Helvetica', 20))
+    crtr.place(x=40,y=5)
+    usrnmr_.place(x=20,y=10)
+    pss_.place(x=20,y=70)
+    Bttn.place(x=50,y=130)
+    Bttn2.place(x=250,y=130)
+    user1.place(x=185,y=15)
+    pass1.place(x=185,y=75)
+    #login function
+    app.mainloop()
 
-crtr.place(x=40,y=5)
-usrnmr_.place(x=20,y=10)
-pss_.place(x=20,y=70)
-Bttn.place(x=50,y=130)
-Bttn2.place(x=250,y=130)
-user1.place(x=185,y=15)
-pass1.place(x=185,y=75)
+def scene2():
+    '''
+        This scene will open a dashboard that will show saved ID card and create new id button
+    '''
 
-#login function
-
-app.mainloop()
+def scene3():
+    '''
+        this scene will generated id and show option to modify and delete
+    '''
+scene1()
 
 
 
